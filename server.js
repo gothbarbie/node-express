f2717138ad9d9457536e27f7943ff264995c6dda
Hanna Söderström <gothbarbie84@gmail.com>
2017-09-23 15:42:18 +0200

    Adds home page and dynamic values to about page

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-// Public folder directory
-app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public')) // Public folder directory
 
+// Routing
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to this website.'
+  })
 })
 
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+
+// Run!
 app.listen(3000, () => console.log('Server running on port 3000.'))
