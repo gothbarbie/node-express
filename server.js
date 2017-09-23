702a0395ffe3a6788667c0a9c512f5c8e0b36f6c
Hanna Söderström <gothbarbie84@gmail.com>
2017-09-23 15:30:36 +0200

    some html and styling

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 
 const app = express()
 
+// Public folder directory
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.send('Hello Express')
 })
+
+app.listen(3000, () => console.log('Server running on port 3000.'))
